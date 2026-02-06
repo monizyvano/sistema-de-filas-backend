e003cff8653c48ee55f38bf15acf24590e264e2d
monizyvano <monizyvano2@gmail.com>
2026-02-06 01:34:49 +0100

    actualização do run
    com socket

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
-from app import create_app
+from app import create_app, socketio
 
+# Criar aplicação
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Rodar com SocketIO para suporte a WebSockets
+    socketio.run(
+        app,
+        debug=True,
+        host='0.0.0.0',
+        port=5000
+    )
